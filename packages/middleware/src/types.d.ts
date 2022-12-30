9497ba1d3c114c1d6d104fe3e146275640c1fb4f
Jascha Schiffer <coding@jschiffer.name>
2022-12-30 14:45:59 +0100

    bunch of small fixes

--- packages/middleware/src/types.d.ts
+++ packages/middleware/src/types.d.ts
@@ -1,6 +1,6 @@
 export * from './index'
 
 // eslint-disable-next-line no-restricted-exports
-export type {default} from './index'
+export {default} from './index'
 
 export {CreateMiddlewareStackOptions} from './types/options'
