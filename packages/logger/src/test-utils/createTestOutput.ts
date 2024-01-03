74b845c001a7ef9b58a97df8032b288b948f6c31
Jascha Schiffer <coding@jschiffer.name>
2024-01-03 16:02:40 +0100

    fix(logger): proper exposure of test utils since subpath export do not seem to work

--- packages/logger/src/test-utils/createTestOutput.ts
+++ packages/logger/src/test-utils/createTestOutput.ts
@@ -1,4 +1,4 @@
-import {Writable as WritableStream} from 'node:stream'
+import {type Writable as WritableStream} from 'node:stream'
 import {WritableMemoryStream} from './WritableMemoryStream'
 
 export const createTestOutput = (): WritableStream & {
--- packages/logger/src/test-utils/index.ts
+++ packages/logger/src/test-utils/index.ts
@@ -1,2 +1,8 @@
+import {toHaveLogged} from './matchers'
+
 export * from './createTestOutput'
 export * from './matchers'
+
+if (typeof expect !== 'undefined') {
+  expect.extend({toHaveLogged})
+}
